0922aa06825e77ca7358c4ad8a14fab4c76f41c5
Nachtel <bertran@versamedium.com>
2020-02-02 22:35:03 -0800

    Added updated archive.py file

--- archiver.py
+++ archiver.py
@@ -1,17 +1,24 @@
-from seletest import *
+from scanner import *
 import pandas as pd
 
-def print_data(i=None):
+
+
+
+def print_data():
+    item_numb = 0
+    row_num = 2
     for item in row_dict:
-        obj = row_dict["{}, row_{}".format(listing_name, row_num)]
-        obj.listing_name
-        obj.r_date
-        obj.r_close
-        obj.r_change
-        obj.r_changepercent
-        obj.r_opxn
-        obj.r_high
-        obj.r_low
-        obj.r_volume
+        obj = row_dict["{}, row_{}".format(identity[item_numb], row_num)]
+        print('Name:', obj.name)
+        print('Date:', obj.date)
+        print('Close:', obj.close)
+        print('Change:', obj.change)
+        print('Change Percentage:', obj.changepercent)
+        print('Open:', obj.opxn)
+        print('High:', obj.high)
+        print('Low:', obj.low)
+        print('Volume:', obj.volume)
         print("-----------------")
+        item_numb +=1
+        row_num +=1
         #print(item, "'s value is {}".format(row_dict[item]))
